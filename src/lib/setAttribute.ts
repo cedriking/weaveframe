dedba168f51e0d2ffe363fed2987780430285509
Cedrik <cedrik@arweave.org>
2020-06-18 16:06:25 -0500

    :art: code format update

--- src/lib/setAttribute.ts
+++ src/lib/setAttribute.ts
@@ -12,21 +12,16 @@ export const setAttribute = (dom: any, key: string, value: any) => {
     dom.removeEventListener(evType, dom.__weaveframeHandles[evType]);
     dom.__weaveframeHandles[evType] = value;
     dom.addEventListener(evType, dom.__weaveframeHandles[evType]);
-
   } else if (key === 'checked' || key === 'value' || key === 'className') {
     // @ts-ignore
     dom[key] = value;
-
   } else if (key === 'style' && typeof value === 'object') {
     Object.assign(dom.style, value);
-
   } else if (key === 'ref' && typeof value === 'function') {
     value(dom);
-
   } else if (key === 'key') {
     dom.__weaveframeKey = value;
-
-  } else if ((typeof value !== 'object' && typeof value !== 'function') && dom.setAttribute) {
+  } else if (typeof value !== 'object' && typeof value !== 'function' && dom.setAttribute) {
     dom.setAttribute(key, value);
   }
 };
--- src/weaveframe.ts
+++ src/weaveframe.ts
@@ -2,5 +2,5 @@ import { createElement } from './lib/createElement';
 import { render } from './lib/render';
 import { Component } from './lib/component';
 
-const WeaveFrame = {createElement, render, Component};
+const WeaveFrame = { createElement, render, Component };
 export default WeaveFrame;
